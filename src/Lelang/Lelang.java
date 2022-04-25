d2ec8a566a625f7c45b5dbafb8c63883d0e73933
unknown <afi.firda@gmail.com>
2022-04-25 08:46:44 +0700

    add - lelang

--- src/Lelang/Lelang.java
+++ src/Lelang/Lelang.java
@@ -1,5 +1,21 @@
 package Lelang;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lelang {
 
+	private ArrayList<Integer> idPenawar = new ArrayList<>();
+	private ArrayList<Integer> idBarang = new ArrayList<>();
+	private ArrayList<Integer> hargaTawar = new ArrayList<>();
+	private ArrayList<String> temp = new ArrayList<>();
+	boolean exitLelang = true;
+	boolean exitProsesLelang = true;
+
+	int max = 0;
+	Scanner scanner = new Scanner(System.in);
+	
+	public void prosesLelang(Barang barang, Petugas petugas, Masyarakat masyarakat, int idx) {
+		
+	}
 }
